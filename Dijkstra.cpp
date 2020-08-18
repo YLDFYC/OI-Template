66da4843fe1a9224f42c5908170b9225442079da
Robert Luo <1015606011@qq.com>
2020-08-18 21:29:33 +0800

    create Dijkstra

---
+++ Dijkstra.cpp
@@ -0,0 +1,23 @@
+//:~ @author RLDF
+//s: 起始点
+//dis: 最短路
+//MAXN: 最大点数
+inline void dijkstra(int s, int *dis, int *vis, priority_queue<pair<int, int>> &q, vector<int> G[MAXN]) {
+    memset(dis, 0x3f, sizeof dis);
+    memset(vis, 0, sizeof vis);
+    dis[s] = 0;
+    q.push({0, s});
+    while (q.size()) {
+        int x = q.top().second;
+        if (vis[x])
+            continue;
+        vis[x] = 1;
+        for (auto i : G[x]) {
+            int y = i.first, w = i.second;
+            if (dis[y] > dis[x] + w) {
+                dis[y] = dis[x] + w;
+                q.push({-dis[y], y});
+            }
+        }
+    }
+}
